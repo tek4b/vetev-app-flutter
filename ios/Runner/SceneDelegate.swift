890c69896a31f143a4f441498aaced3f998785b0
tek4b <bruno.ferreira.tadim@gmail.com>
2026-07-05 10:32:10 +0100

    Update SceneDelegate.swift

--- ios/Runner/SceneDelegate.swift
+++ ios/Runner/SceneDelegate.swift
@@ -1,6 +1,18 @@
 import Flutter
 import UIKit
 
-class SceneDelegate: FlutterSceneDelegate {
+class SceneDelegate: UIResponder, UIWindowSceneDelegate {
+  var window: UIWindow?
+  let engine = FlutterEngine(name: "vetev_engine")
 
+  func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options: UIScene.ConnectionOptions) {
+    engine.run()
+    GeneratedPluginRegistrant.register(with: engine)
+
+    guard let windowScene = scene as? UIWindowScene else { return }
+    window = UIWindow(windowScene: windowScene)
+    let vc = FlutterViewController(engine: engine, nibName: nil, bundle: nil)
+    window?.rootViewController = vc
+    window?.makeKeyAndVisible()
+  }
 }
