d48444d8d2d0c3aea59465ce2203d230b497bc23
tek4b <bruno.ferreira.tadim@gmail.com>
2026-07-11 17:12:00 +0100

    Update SceneDelegate.swift

--- ios/Runner/SceneDelegate.swift
+++ ios/Runner/SceneDelegate.swift
@@ -2,5 +2,14 @@ import Flutter
 import UIKit
 
 class SceneDelegate: FlutterSceneDelegate {
+  var window: UIWindow?
 
+  override func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options: UIScene.ConnectionOptions) {
+    guard let windowScene = scene as? UIWindowScene else { return }
+
+    let vc = FlutterViewController()
+    window = UIWindow(windowScene: windowScene)
+    window?.rootViewController = vc
+    window?.makeKeyAndVisible()
+  }
 }
