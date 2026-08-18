c1fb970aa433021a4b66ae2a5e15590e36f9a488
tek4b <bruno.ferreira.tadim@gmail.com>
2026-08-18 12:26:11 +0100

    Recria pasta ios do zero + icone

--- ios/Runner/SceneDelegate.swift
+++ ios/Runner/SceneDelegate.swift
@@ -2,12 +2,5 @@ import Flutter
 import UIKit
 
 class SceneDelegate: FlutterSceneDelegate {
-  override func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options: UIScene.ConnectionOptions) {
-    guard let windowScene = scene as? UIWindowScene else { return }
 
-    let vc = FlutterViewController()
-    window = UIWindow(windowScene: windowScene)
-    window?.rootViewController = vc
-    window?.makeKeyAndVisible()
-  }
 }
